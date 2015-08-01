4f8ea04cf6798f420560304c5b0ce8209a8d4f4c
James Youn <jamesyoun710@gmail.com>
2015-07-31 21:32:53 -0700

    add dashboard and shouts

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -2,7 +2,13 @@ class ShoutsController < ApplicationController
   def create
     shout = current_user.shouts.build(shout_params)
     shout.save
-    redirect_to dashboard_path
+
+    if shout.save
+      redirect_to dashboard_path
+    else
+      flash.alert = "Could not shout."
+      redirect_to dashboard_path
+    end
   end
 
   def show
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @shouts = @user.shouts
+  end
+
+  private
+end
