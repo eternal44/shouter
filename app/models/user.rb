fcd90de2d9f6b0d15372624e98f9cf7f501cde13
James Youn <jamesyoun710@gmail.com>
2015-08-02 17:56:34 -0700

    extract timeline features from dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,10 +21,12 @@
 #
 
 class User < ActiveRecord::Base
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :shouts
 
   has_many :followed_user_relationships, 
@@ -36,4 +38,16 @@ class User < ActiveRecord::Base
     foreign_key: :followed_user_id, 
     class_name: 'FollowingRelationship'
   has_many :followers, through: :follower_relationships
+
+  def following? user
+    followed_user_ids.include? user.id
+  end
+
+  def follow user
+    followed_users << user
+  end
+
+  def unfollow user
+    followed_users.delete(user)
+  end
 end
